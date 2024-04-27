c1826fd41a599c76c5dc5101cdbcd279c0b28861
Sabin <127272916+Sabin404@users.noreply.github.com>
2024-04-27 23:04:40 +0545

    Update xorGate_tb.vhdl

--- CA/xorGate_tb.vhdl
+++ CA/xorGate_tb.vhdl
@@ -1,5 +1,4 @@
-To create a testbench for XOR VHDL file.
-Source code:
+
 library ieee;
 use ieee.std_logic_1164.all;
 entity xorGate_tb is 
@@ -13,7 +12,7 @@ c : out std_logic
 end component;
 signal a,b,c : std_logic; 
 begin
-nandGate_inst: nandGate port map 
+xorGate_inst: xorGate port map 
 ( a => a, b => b, c => c);
 process begin
 a <= '0';
@@ -24,5 +23,11 @@ b <= '1';
 wait for 1 ns;
 a <= '1';
 b <= '0';
-wait for 1 ns; a <= '1'b <= '1';wait for 1 ns;
-assert false report "successfully completed"; wait;end process; end test
+wait for 1 ns; 
+a <= '1';
+b <= '1';
+wait for 1 ns;
+assert false report "successfully completed"; 
+wait;
+end process; 
+end test;
